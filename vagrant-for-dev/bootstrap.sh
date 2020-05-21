6513090da3d5c3e2afe1a479e5709df79d745d62
david <qustwwy@163.com>
2020-05-21 21:53:55 +0800

    doc

--- vagrant-for-dev/bootstrap.sh
+++ vagrant-for-dev/bootstrap.sh
@@ -3,9 +3,9 @@ set -vx
 
 sed -ri "s/(archive|security).ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list 
 apt-get update
-apt-get install -y apache2
+apt-get install -y nginx
 
-if ! [ -L /var/www ]; then
-    rm -rf /var/www
-    ln -fs /vagrant /var/www
+if ! [ -L /var/www/html ]; then
+    rm -rf /var/www/html
+    ln -fs /vagrant /var/www/html
 fi
